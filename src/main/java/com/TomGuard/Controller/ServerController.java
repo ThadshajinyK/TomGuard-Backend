66f2a3253c6f0cb8d856b1ffe30883da3a2f4828
Ragavarshithan <105372507+Ragavarshithan@users.noreply.github.com>
2023-06-18 12:06:42 +0530

    server pdf generation part added

--- src/main/java/com/TomGuard/Controller/ServerController.java
+++ src/main/java/com/TomGuard/Controller/ServerController.java
@@ -1,11 +1,19 @@
 package com.TomGuard.Controller;
 
 import com.TomGuard.Entity.ServerEntity;
+import com.TomGuard.Entity.ServerPdf;
+import com.TomGuard.Repository.ServerRepo;
 import com.TomGuard.Service.ServerService;
+import com.lowagie.text.DocumentException;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,5 +52,26 @@ public class ServerController {
     public void deleteServer(@PathVariable("id") Long id)
     {
         serverService.deleteServer(id);
+    }
+
+    @GetMapping("/serverpdf")
+    public void generatePdf(HttpServletResponse response) throws DocumentException, IOException {
+
+        response.setContentType("application/pdf");
+        DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-DD:HH:MM:SS");
+        String currentDateTime = dateFormat.format(new Date());
+        String headerkey = "Content-Disposition";
+        String headervalue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
+        response.setHeader(headerkey, headervalue);
+
+        List<ServerEntity> serverEntityList = serverService.findAllServers();
+
+
+        ServerPdf serverPdf = new ServerPdf();
+        serverPdf.setserverlist(serverEntityList);
+        serverPdf.generate(response);
+
+
+
     }
 }
--- src/main/java/com/TomGuard/Repository/ServerRepo.java
+++ src/main/java/com/TomGuard/Repository/ServerRepo.java
@@ -4,8 +4,11 @@ import com.TomGuard.Entity.ServerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ServerRepo extends JpaRepository<ServerEntity, Long> {
+
 
 }
