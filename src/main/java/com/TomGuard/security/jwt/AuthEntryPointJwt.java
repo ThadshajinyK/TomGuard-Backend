999b37ff6ede266a2a1b51e8f379457b1694c965
ThadshajinyK <kaneshanthadshajiny@gmail.com>
2023-07-11 18:27:16 +0530

    all metrics attributes are modified

--- src/main/java/com/TomGuard/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/TomGuard/security/jwt/AuthEntryPointJwt.java
@@ -25,14 +25,17 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
       throws IOException, ServletException {
     logger.error("Unauthorized error: {}", authException.getMessage());
 
+
     response.setContentType(MediaType.APPLICATION_JSON_VALUE);
     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
 
     final Map<String, Object> body = new HashMap<>();
     body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
     body.put("error", "Unauthorized");
     body.put("message", authException.getMessage());
     body.put("path", request.getServletPath());
+
 
     final ObjectMapper mapper = new ObjectMapper();
     mapper.writeValue(response.getOutputStream(), body);
--- src/main/java/com/TomGuard/security/jwt/JwtUtils.java
+++ src/main/java/com/TomGuard/security/jwt/JwtUtils.java
@@ -24,8 +24,6 @@ public class JwtUtils {
 
     UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
-
-
     return Jwts.builder()
         .setSubject((userPrincipal.getEmail()))
         .setIssuedAt(new Date())
